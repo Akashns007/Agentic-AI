7c889bdb40b471568ad04c4e482457eb75d02eb3
Akashns007 <mrpeculiar007@gmail.com>
2024-12-12 16:33:58 +0530

    second commit

--- Agents/MarkdownTools.py
+++ Agents/MarkdownTools.py
@@ -1,31 +1,32 @@
 import os
-import mistune
+import sys
 from langchain.tools import tool
+from pymarkdown.api import PyMarkdownApi, PyMarkdownApiException
 
 @tool("markdown_validation_tool")
 def markdown_validation_tool(file_path: str) -> str:
     """
-    A tool to review files for markdown syntax validity.
+    A tool to review files for markdown syntax errors.
 
     Parameters:
     - file_path: The path to the markdown file to be reviewed.
 
     Returns:
-    - validation_results: A list of potential syntax issues.
+    - validation_results: A list of validation results 
+    and suggestions on how to fix them.
     """
-    if not os.path.exists(file_path):
-        return "Could not validate file. The provided file path does not exist."
+    
+    print("\n\nValidating Markdown syntax...\n\n" + file_path)
 
+    scan_result = None
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            content = file.read()
+        if not (os.path.exists(file_path)):
+           return "Could not validate file. The provided file path does not exist."
 
-        # Use Mistune to parse Markdown
-        parser = mistune.create_markdown()
-        html_output = parser(content)  # Convert to HTML
-        if html_output:
-            return "Markdown syntax appears valid."
-        else:
-            return "The Markdown file might be empty or invalid."
-    except Exception as e:
-        return f"Error during validation: {str(e)}"
+        scan_result = PyMarkdownApi().scan_path(file_path.rstrip().lstrip())
+        results = str(scan_result)    
+        return results  # Return the reviewed document
+    except PyMarkdownApiException as this_exception:
+        print(f"API Exception: {this_exception}", file=sys.stderr)
+        return f"API Exception: {str(this_exception)}"
+  
